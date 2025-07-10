7d2c5e75d504a70786b2e98ac393781dc4b1c424
Andy Aschwanden <andy.aschwanden@gmail.com>
2025-07-10 15:47:56 -0800

    Bumped version:
    PISM -> 2.2.2
    YAC -> 3.7.1
    YAXT -> 0.11.4

---
+++ recipes/pism-dbg/build.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+set -ex
+
+if [[ "${target_platform}" == linux-* ]]; then
+    export LDFLAGS="-pthread -fopenmp ${LDFLAGS}"
+    export LDFLAGS="${LDFLAGS} -Wl,-rpath-link,${PREFIX}/lib"
+fi
+
+optimization_flags="-O3"
+
+export CC="mpicc"
+export CXX="mpicxx"
+
+
+cmake -D CMAKE_CXX_FLAGS="${optimization_flags}" \
+      -D CMAKE_C_FLAGS="${optimization_flags}" \
+      -D CMAKE_PREFIX_PATH="${PREFIX}" \
+      -D CMAKE_INSTALL_PREFIX="${PREFIX}" \
+      -D CMAKE_INSTALL_LIBDIR=lib \
+      -D Python3_EXECUTABLE=${PREFIX}/bin/python \
+      -D Pism_DEBUG=YES \
+      -D Pism_BUILD_PYTHON_BINDINGS=YES \
+      -D Pism_ENABLE_DOCUMENTATION=NO \
+      -D Pism_PKG_CONFIG_STATIC=NO \
+      -D Pism_USE_JANSSON=NO \
+      -D Pism_USE_PARALLEL_NETCDF4=YES \
+      -D Pism_USE_PROJ=YES \
+      -D Pism_USE_YAC_INTERPOLATION=YES \
+      "${SRC_DIR}"
+
+
+make -j"${CPU_COUNT}"
+
+make install
